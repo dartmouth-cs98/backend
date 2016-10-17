0fc9042adc2b13048d12eba9340ee53f14ad51be
Zachary Tannenbaum <zach@sighten.io>
2016-10-17 00:06:22 -0400

    setup django further

--- history/urls.py
+++ history/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from history import views
 
 urlpatterns = [
-    url(r'^pages/$', views.page_list),
-    url(r'^pages/(?P<id>[0-9]+)/$', views.page_detail),
+    url(r'^pages/$', views.PageList.as_view()),
+    url(r'^pages/(?P<id>[0-9]+)/$', views.PageDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
