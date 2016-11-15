b134cc493061905364f201e4871a1c0eaf35cb55
gracemiller <grace.a.miller.17@dartmouth.edu>
2016-11-15 15:20:32 -0500

    created admin page with domains, useful with editing backend

--- history/admin.py
+++ history/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class DomainAdmin(admin.ModelAdmin):
+	list_display = ['title', 'created', 'closed', 'tab']
+	ordering = ['-created']
+
+admin.site.register(Domain, DomainAdmin)
+admin.site.register(Tab)
+admin.site.register(Page)
+admin.site.register(PageVisit)
