98253ac71acd2f226c13526d305ad84c0f813ffa
Zachary Tannenbaum <zach@sighten.io>
2017-01-07 19:03:47 -0500

    revert back to pre-CORS errors

--- hindsite/settings.py
+++ hindsite/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 import os
 import dj_database_url
 from django.core.exceptions import ImproperlyConfigured
-from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,13 +73,6 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_WHITELIST = [
     'chrome-extension://ocpchccceiphbojcehihihigmoppoflg',
 ]
-
-CORS_ALLOW_METHODS = (
-    'GET',
-    'POST',
-)
-
-CORS_ALLOW_HEADERS = default_headers
 
 ROOT_URLCONF = 'hindsite.urls'
 
