eafa2d71cd3d4471520292b71421276896a0f16f
trk17 <trk17@users.noreply.github.com>
2017-02-21 17:30:11 -0500

    Actives also leading to timeouts
    Endpoint Hanging Bug Hunt

--- history/urls.py
+++ history/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     url(r'^updatestar/$', update_categories.UpdateStar.as_view()),
 
     #url(r'^newpage/$', newpage_views.NewPage.as_view()),
-    url(r'^active/$', newpage_views.UpdateActive.as_view()),
+    #url(r'^active/$', newpage_views.UpdateActive.as_view()),
 
     url(r'^tabinfo/$', send_data.SendTabs.as_view()),
     url(r'^domaininfo/$', send_data.SendDomain.as_view()),
